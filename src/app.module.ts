b3479e6ef1c55f1e3d9bbe654f022680600f7e02
Siser Pratap <siserinsevoc@gmail.com>
2025-02-05 15:40:32 +0530

    Updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 @Module({
   imports: [MongooseModule.forRoot(`${process.env.MONGO_URI}`)],
